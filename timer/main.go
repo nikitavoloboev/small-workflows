4392faec7cbe0b092d1afeb57344508cc2d0be24
Nikita Voloboev <nikita.voloboev@gmail.com>
2018-08-09 16:09:26 +0200

    update

---
+++ timer/main.go
@@ -0,0 +1,8 @@
+package main
+
+// TODO: render the time on screen using HS, not Alfred as it's fast. save the text to render to a file somewhere that HS can then read (bind g+s to show time)
+// TODO: write helpers to format the time remaining nicely (use eggtimer code)
+
+func main() {
+	
+}

46bfd568c82ce6f8ecd4ca59c122102ec78a444c
Nikita Voloboev <nikita.voloboev@gmail.com>
2019-01-24 12:35:00 +0100

    update

--- timer/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-// TODO: write helpers to format the time remaining nicely (use eggtimer code)
-
-// TODO: output time left so I can then display it nicely on the TouchBar or elsewhere
-
-func main() {
-	
-}
